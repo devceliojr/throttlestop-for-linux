03d2078f725d89146acbe68b12818eb065e57a7a
Célio Junior <74278014+devceliojr@users.noreply.github.com>
2023-03-23 15:03:47 -0300

    Update throttlestop.sh

--- bin/throttlestop.sh
+++ bin/throttlestop.sh
@@ -3,10 +3,10 @@
 MAX_CPU=$((`nproc --all` - 1));
 
 for i in $(seq 0 $MAX_CPU); do
-    
-    -set -c $i -g performance;
+    cpufreq-set -c $i -g performance;
 done
 
-modprobe msr
-wrmsr -a 0x1a0 0x850089
-echo 0 | tee /sys/devices/system/cpu/intel_pstate/no_turbo >/dev/null
+modprobe msr;
+wrmsr -a 0x1a0 0x850089;
+wrmsr 0x1FC $((`rdmsr -d 0x1FC` - 1));
+echo 0 | tee /sys/devices/system/cpu/intel_pstate/no_turbo >/dev/null;
