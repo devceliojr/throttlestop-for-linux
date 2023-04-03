1e12f7533e454dd94506d28227ff98b32c33bee4
Célio Junior <74278014+devceliojr@users.noreply.github.com>
2023-04-02 21:57:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install sudo wget neofetch msr-tools cpufrequtils -y;
+sudo apt install sudo wget msr-tools cpufrequtils -y;
 
 wget https://raw.githubusercontent.com/devceliojr/throttlestop-for-linux/main/bin/throttlestop.sh -O /usr/bin/throttlestop;
 wget https://raw.githubusercontent.com/devceliojr/throttlestop-for-linux/main/bin/throttlestop.service -O /etc/systemd/system/throttlestop.service;
@@ -13,4 +13,3 @@ sudo systemctl daemon-reload;
 sudo systemctl enable throttlestop.service;
 /usr/bin/throttlestop;
 clear;
-neofetch;
