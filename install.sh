ba3371c6f84cbfa4136bcaa56a50cd4d98b5f637
Célio Junior <74278014+devceliojr@users.noreply.github.com>
2023-04-02 22:05:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-#!/bin/bashsudo apt install sudo wget msr-tools cpufrequtils -y;wget https://raw.githubusercontent.com/devceliojr/throttlestop-for-linux/main/bin/throttlestop.sh -O /usr/bin/throttlestop;wget https://raw.githubusercontent.com/devceliojr/throttlestop-for-linux/main/bin/throttlestop.service -O /etc/systemd/system/throttlestop.service;dos2unix /usr/bin/throttlestop;dos2unix /etc/systemd/system/throttlestop.service;sudo chmod +x /usr/bin/throttlestop;sudo systemctl daemon-reload;sudo systemctl enable throttlestop.service;/usr/bin/throttlestop;clear;
+#!/bin/bash
+sudo apt install sudo wget msr-tools cpufrequtils -y;
+wget https://raw.githubusercontent.com/devceliojr/throttlestop-for-linux/main/bin/throttlestop.sh -O /usr/bin/throttlestop;
+wget https://raw.githubusercontent.com/devceliojr/throttlestop-for-linux/main/bin/throttlestop.service -O /etc/systemd/system/throttlestop.service;
+
+dos2unix /usr/bin/throttlestop;
+dos2unix /etc/systemd/system/throttlestop.service;
+
+sudo chmod +x /usr/bin/throttlestop;
+sudo systemctl daemon-reload;
+sudo systemctl enable throttlestop.service;
+/usr/bin/throttlestop;
+clear;
